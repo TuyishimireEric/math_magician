ec9c12e4d8712cbdd7cd5566437ba4e63a36acf2
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-09-02 14:43:07 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,9 @@ function Home() {
       <h2>Welcome to our Home page!</h2>
       <p className="welcome">
         {' '}
-
-       A calculator is a machine which allows people to do math operations more easily
-       this web calculator is able to perfom the Basic calculations such as addition,
-         subtraction, multiplication and division mathematical calculations.
+        A calculator is a machine which allows people to do math operations more easily
+        this web calculator is able to perfom the Basic calculations such as addition,
+        subtraction, multiplication and division mathematical calculations.
       </p>
     </main>
 
@@ -17,3 +16,4 @@ function Home() {
 }
 
 export default Home;
+
